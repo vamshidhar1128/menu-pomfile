660942367c0de614c645f75d92293470d90212fc
vamshidhar1128 <164383145+vamshidhar1128@users.noreply.github.com>
2025-05-07 12:46:20 +0530

    Update UserApplicationTests.java

--- src/test/java/com/business/www/UserApplicationTests.java
+++ src/test/java/com/business/www/UserApplicationTests.java
@@ -1,13 +1,32 @@
-package com.business.www;
+// package com.business.www;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UserApplicationTests {
+// @SpringBootTest
+// class UserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
+// }
+package com.bhanu.empid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class UserApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UserApplication.class);
+    }
 }
